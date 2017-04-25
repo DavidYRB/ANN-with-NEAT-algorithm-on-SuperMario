85a5739c8af46f5fe572f5e62be3c2b54704c3c3
DavidYRB <david.yanrb@gmail.com>
2017-04-24 22:50:04 -0400

    add new files

---
+++ Game_Play.m
@@ -0,0 +1,3 @@
+playSound = questdlg('Sound?','','Yes');
+playSound = strcmp(playSound, 'Yes');
+Attri_FBF = stl_mario_main(playSound);
---
+++ find_MarioView.m
@@ -0,0 +1,3 @@
+function MarioView = find_MarioView(x,y)
+    MarioView = binary_mapIdx(x-2:x+2,y-2:y+2);
+end
